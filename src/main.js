4770877741c504d115f4f6bbe0dcae43d0397e97
Kunal <kunals248@eng.rizvi.edu.in>
2023-10-22 16:01:45 +0530

    Adding existing project files

---
+++ src/main.js
@@ -0,0 +1,80 @@
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x; // destructing
+      let search = basket.find((x) => x.id === id) || [];
+      return `        
+        <div id=product-id-${id} class="item">
+            <img width="227" src="${img}" alt="">
+            <div class="details">
+                <h3>${name}</h3>
+                <p>${desc}</p>
+                <div class="price-quantity">
+                    <h2 class="pro-price">$ ${price}</h2>
+                    <div class="buttons">
+                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">
+                        ${search.item === undefined ? 0 : search.item}
+                        </div>
+                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    })
+    .join(""));
+};
+generateShop();
+
+// increment********************
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+// decrement ******************
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined || search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0); // this function removes the item which has 0 value init.
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket
+    .map((x) => x.item)
+    .reduce((acc, item) => acc + item, 0);
+};
+calculation();
